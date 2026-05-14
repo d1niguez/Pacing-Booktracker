e380071ee408841690bcef59e8c589ac7170ffcf
Danna <iniguezdanna0523@gmail.com>
2026-05-14 18:47:48 -0500

    connected books to Flask and Jinja (page numbers did not change, still testing!)

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+books = [
+    {"title": "The Lovely Bones", "author": "Alice Sebold", "current_page": 49, "total_pages": 500},
+    {"title": "A Court of Thorns and Roses", "author": "Sarah J. Maas", "current_page": 90, "total_pages": 500},
+    {"title": "The Fault in Our Stars", "author": "John Green", "current_page": 55, "total_pages": 500},
+    {"title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "current_page": 55, "total_pages": 500},
+]
+
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('dashboard.html',books=books)
 
 if __name__ == '__main__':
     app.run(debug=True)
